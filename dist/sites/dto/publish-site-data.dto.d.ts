5d47e363005f0bd9940d82b7d7f510468cd9f6c2
Bhalli Bhai <94342592+BhalliBhai@users.noreply.github.com>
2023-03-28 01:37:09 +0500

    app.yaml

---
+++ dist/sites/dto/publish-site-data.dto.d.ts
@@ -0,0 +1,10 @@
+export declare class PublishSiteDto {
+    readonly siteUrl: string;
+    readonly published: boolean;
+    readonly siteName: string;
+    readonly siteDescription: string;
+    readonly siteThumbnail: string;
+    readonly siteType: string;
+    readonly tokenGate: boolean;
+    readonly chainIds: Array<number>;
+}
